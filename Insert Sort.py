65f49db0d1380c25f5f01c980488d1cfee78e877
FinancialMan <78961137+FinancialMan@users.noreply.github.com>
2021-03-01 09:49:55 +0100

    Update Insert Sort.py

--- Insert Sort.py
+++ Insert Sort.py
@@ -2,12 +2,12 @@ A = [50, 20000, 1, 88778, 5, 121, 244, 60]
 
 def insert_sort(A):
     for i in range(1, len(A)):
-        klucz = A[i]
+        key = A[i]
         j = i-1
-        while j >= 0 and A[j]>klucz:
+        while j >= 0 and A[j]>key:
             A[j+1] = A[j]
             j = j-1
-        A[j+1] = klucz
+        A[j+1] = key
     return A
 
 a = insert_sort(A)
